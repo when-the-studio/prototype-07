72671203f426d2fa229984e1f2d68bf26154dcf1
anima-libera <anim.libera@gmail.com>
2023-07-03 02:17:00 +0200

    Window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,94 @@
 fn main() {
-	println!("Hello, world!");
+	env_logger::init();
+	let event_loop = winit::event_loop::EventLoop::new();
+
+	let grid_w = 20;
+	let grid_h = 20;
+	let cell_pixel_side = 8 * 4;
+
+	let window = winit::window::WindowBuilder::new()
+		.with_title("Prototype 7")
+		.with_inner_size(winit::dpi::PhysicalSize::new(
+			(grid_w * cell_pixel_side) as u32,
+			(grid_h * cell_pixel_side) as u32,
+		))
+		.build(&event_loop)
+		.unwrap();
+
+	// Center the window
+	let screen_size = window.available_monitors().next().unwrap().size();
+	let window_outer_size = window.outer_size();
+	window.set_outer_position(winit::dpi::PhysicalPosition::new(
+		screen_size.width / 2 - window_outer_size.width / 2,
+		screen_size.height / 2 - window_outer_size.height / 2,
+	));
+
+	// Set background and edge color
+	let clear_color = [0, 50, 0, 255];
+	let clear_color_wgpu = {
+		fn conv_srgb_to_linear(x: f64) -> f64 {
+			// See https://github.com/gfx-rs/wgpu/issues/2326
+			// Stolen from https://github.com/three-rs/three/blob/07e47da5e0673aa9a16526719e16debd59040eec/src/color.rs#L42
+			// (licensed MIT, not a substancial portion so not concerned by license obligations)
+			// Basically the brightness is adjusted somewhere by wgpu or something due to sRGB stuff,
+			// color is hard.
+			if x > 0.04045 {
+				((x + 0.055) / 1.055).powf(2.4)
+			} else {
+				x / 12.92
+			}
+		}
+		pixels::wgpu::Color {
+			r: conv_srgb_to_linear(clear_color[0] as f64 / 255.0),
+			g: conv_srgb_to_linear(clear_color[1] as f64 / 255.0),
+			b: conv_srgb_to_linear(clear_color[2] as f64 / 255.0),
+			a: conv_srgb_to_linear(clear_color[3] as f64 / 255.0),
+		}
+	};
+
+	let mut pixel_buffer_size = window.inner_size();
+	let mut pixel_buffer = {
+		let size = pixel_buffer_size;
+		let surface_texture = pixels::SurfaceTexture::new(size.width, size.height, &window);
+		pixels::PixelsBuilder::new(size.width, size.height, surface_texture)
+			.clear_color(clear_color_wgpu)
+			.build()
+			.unwrap()
+	};
+
+	let spritesheet = image::load_from_memory(include_bytes!("../assets/spritesheet.png")).unwrap();
+
+	use winit::event::*;
+	event_loop.run(move |event, _, control_flow| match event {
+		Event::WindowEvent { ref event, window_id } if window_id == window.id() => match event {
+			WindowEvent::CloseRequested
+			| WindowEvent::KeyboardInput {
+				input:
+					KeyboardInput {
+						state: ElementState::Pressed,
+						virtual_keycode: Some(VirtualKeyCode::Escape),
+						..
+					},
+				..
+			} => {
+				*control_flow = winit::event_loop::ControlFlow::Exit;
+			},
+			_ => {},
+		},
+
+		Event::MainEventsCleared => {
+			std::thread::sleep(std::time::Duration::from_millis(7));
+			pixel_buffer
+				.frame_mut()
+				.chunks_exact_mut(4)
+				.for_each(|pixel| pixel.copy_from_slice(&clear_color));
+			window.request_redraw();
+		},
+
+		Event::RedrawRequested(_) => {
+			pixel_buffer.render().unwrap();
+		},
+
+		_ => {},
+	});
 }
