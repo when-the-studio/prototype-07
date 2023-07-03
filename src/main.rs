fd6a00d979c744338134fa1477c4d235d4518b09
anima-libera <anim.libera@gmail.com>
2023-07-03 03:00:55 +0200

    Added objects and controls

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,142 @@
+use image::GenericImageView;
+
+#[derive(Clone)]
+enum Cell {
+	Empty,
+	Player,
+	Goal,
+	Enemy,
+	Tower,
+}
+
+struct Grid<T> {
+	w: i32,
+	h: i32,
+	content: Vec<T>,
+}
+
+impl<T: Clone> Grid<T> {
+	fn new(w: i32, h: i32, value: T) -> Grid<T> {
+		Grid {
+			w: 20,
+			h: 20,
+			content: std::iter::repeat(value).take((w * h) as usize).collect(),
+		}
+	}
+}
+
+impl<T> Grid<T> {
+	fn get(&self, coords: Coords) -> Option<&T> {
+		let index = coords.y * self.w + coords.x;
+		if 0 <= coords.x && coords.x < self.w && 0 <= coords.y && coords.y < self.h {
+			self.content.get(index as usize)
+		} else {
+			None
+		}
+	}
+	fn get_mut(&mut self, coords: Coords) -> Option<&mut T> {
+		let index = coords.y * self.w + coords.x;
+		if 0 <= coords.x && coords.x < self.w && 0 <= coords.y && coords.y < self.h {
+			self.content.get_mut(index as usize)
+		} else {
+			None
+		}
+	}
+}
+
+struct Coords {
+	x: i32,
+	y: i32,
+}
+
+impl From<(i32, i32)> for Coords {
+	fn from((x, y): (i32, i32)) -> Coords {
+		Coords { x, y }
+	}
+}
+
+struct Rect {
+	x: i32,
+	y: i32,
+	w: i32,
+	h: i32,
+}
+
+impl Rect {
+	fn tile(coords: Coords, tiles_side: i32) -> Rect {
+		Rect {
+			x: coords.x * tiles_side,
+			y: coords.y * tiles_side,
+			w: tiles_side,
+			h: tiles_side,
+		}
+	}
+}
+
+fn draw_sprite(
+	pixel_buffer: &mut pixels::Pixels,
+	pixel_buffer_size: winit::dpi::PhysicalSize<u32>,
+	dst: Rect,
+	spritesheet: &image::DynamicImage,
+	sprite: Rect,
+) {
+	// (rx, ry) is a pixel in the dst rect but with (0, 0) being the top left corner
+	for ry in 0..dst.h {
+		for rx in 0..dst.w {
+			// (sx, sy) is the pixel to read from the spritesheet
+			let sx = sprite.x as u32 + rx as u32 * sprite.w as u32 / dst.w as u32;
+			let sy = sprite.y as u32 + ry as u32 * sprite.h as u32 / dst.h as u32;
+			let color = spritesheet.get_pixel(sx, sy).0;
+			if color[3] == 0 {
+				// transparent
+				continue;
+			}
+			// (px, py) is the pixel to write to in the pixel buffer
+			// each of which is visited once
+			let px = rx + dst.x;
+			let py = ry + dst.y;
+			if 0 <= px
+				&& px < pixel_buffer_size.width as i32
+				&& 0 <= py && py < pixel_buffer_size.height as i32
+			{
+				let pixel_index = (py * pixel_buffer_size.width as i32 + px) as usize * 4;
+				pixel_buffer.frame_mut()[pixel_index..(pixel_index + 4)].copy_from_slice(&color);
+			}
+		}
+	}
+}
+
+fn player_move(grid: &mut Grid<Cell>, (dx, dy): (i32, i32)) {
+	for y in 0..grid.h {
+		for x in 0..grid.w {
+			if matches!(grid.get((x, y).into()), Some(Cell::Player)) {
+				if matches!(grid.get((x + dx, y + dy).into()), Some(Cell::Empty)) {
+					*grid.get_mut((x, y).into()).unwrap() = Cell::Empty;
+					*grid.get_mut((x + dx, y + dy).into()).unwrap() = Cell::Player;
+				}
+				return;
+			}
+		}
+	}
+}
+
 fn main() {
 	env_logger::init();
 	let event_loop = winit::event_loop::EventLoop::new();
 
-	let grid_w = 20;
-	let grid_h = 20;
+	let mut grid: Grid<Cell> = Grid::new(20, 20, Cell::Empty);
+	*grid.get_mut((4, 2).into()).unwrap() = Cell::Player;
+	*grid.get_mut((4, 4).into()).unwrap() = Cell::Goal;
+	*grid.get_mut((5, 5).into()).unwrap() = Cell::Tower;
+	*grid.get_mut((6, 8).into()).unwrap() = Cell::Enemy;
+
 	let cell_pixel_side = 8 * 4;
 
 	let window = winit::window::WindowBuilder::new()
 		.with_title("Prototype 7")
 		.with_inner_size(winit::dpi::PhysicalSize::new(
-			(grid_w * cell_pixel_side) as u32,
-			(grid_h * cell_pixel_side) as u32,
+			(grid.w * cell_pixel_side) as u32,
+			(grid.h * cell_pixel_side) as u32,
 		))
 		.build(&event_loop)
 		.unwrap();
@@ -24,7 +150,7 @@ fn main() {
 	));
 
 	// Set background and edge color
-	let clear_color = [0, 50, 0, 255];
+	let clear_color = [0, 50, 50, 255];
 	let clear_color_wgpu = {
 		fn conv_srgb_to_linear(x: f64) -> f64 {
 			// See https://github.com/gfx-rs/wgpu/issues/2326
@@ -73,6 +199,28 @@ fn main() {
 			} => {
 				*control_flow = winit::event_loop::ControlFlow::Exit;
 			},
+
+			WindowEvent::KeyboardInput {
+				input: KeyboardInput { state: ElementState::Pressed, virtual_keycode: Some(key), .. },
+				..
+			} if matches!(
+				key,
+				VirtualKeyCode::Up
+					| VirtualKeyCode::Right
+					| VirtualKeyCode::Down
+					| VirtualKeyCode::Left
+			) =>
+			{
+				let dxdy = match key {
+					VirtualKeyCode::Up => (0, -1),
+					VirtualKeyCode::Right => (1, 0),
+					VirtualKeyCode::Down => (0, 1),
+					VirtualKeyCode::Left => (-1, 0),
+					_ => unreachable!(),
+				};
+				player_move(&mut grid, dxdy);
+			},
+
 			_ => {},
 		},
 
@@ -82,6 +230,30 @@ fn main() {
 				.frame_mut()
 				.chunks_exact_mut(4)
 				.for_each(|pixel| pixel.copy_from_slice(&clear_color));
+
+			for y in 0..grid.h {
+				for x in 0..grid.w {
+					let sprite = match grid.get((x, y).into()).unwrap() {
+						Cell::Empty => None,
+						Cell::Player => Some((0, 0)),
+						Cell::Goal => Some((1, 0)),
+						Cell::Enemy => Some((2, 0)),
+						Cell::Tower => Some((3, 0)),
+					};
+					if let Some(sprite) = sprite {
+						let sprite_rect = Rect::tile(sprite.into(), 8);
+						let dst = Rect::tile((x, y).into(), cell_pixel_side);
+						draw_sprite(
+							&mut pixel_buffer,
+							pixel_buffer_size,
+							dst,
+							&spritesheet,
+							sprite_rect,
+						);
+					}
+				}
+			}
+
 			window.request_redraw();
 		},
 
